df09215f69559fdc4ff6ad92c7bb3b27fa58fb1a
Jerald Calunsag <htcteambangis@gmail.com>
2024-11-10 16:49:27 +0800

    Securing the table booking API Done

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),  # Define URL route for index() view
     path('menu/', views.MenuItemsView.as_view(), name='menu-items'),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token authentication endpoint
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -5,12 +5,14 @@ from .serializers import MenuSerializer
 from rest_framework import viewsets
 from .models import Booking
 from .serializers import BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -21,3 +23,4 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]  # Secure the view
